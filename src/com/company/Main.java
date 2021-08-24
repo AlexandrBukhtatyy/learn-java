dc6fa7a8691e96a5797038a1e3f5bb63726cef04
abukhtatyy <buhtatyalexander90@gmail.com>
2021-08-24 20:50:30 +0300

    basic primitive types

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        byte b = 0; // 1 байт byte(256) -128/+128
+        short sh = 1234; // 2 байта(~65000) -32k/+32k
+        int in = 23423424;  // 4 байта
+        long ln = 234234; // 8 байт
+        float flt = 12.324f; // 4 байта
+        double dbl = 12.324; // 8 байт
+        boolean bln = false; // 1 бит
+        char chr;
         System.out.println("Hello World!");
     }
 }
